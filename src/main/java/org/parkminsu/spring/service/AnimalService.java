76ef35f83356db6af9860a7bb0e5ff4a5c4b1fed
minsu <zmfjtnl1@naver.com>
2024-01-03 20:22:08 +0900

    Upload AnimalService Constructor

--- src/main/java/org/parkminsu/spring/service/AnimalService.java
+++ src/main/java/org/parkminsu/spring/service/AnimalService.java
@@ -6,7 +6,8 @@ import org.parkminsu.spring.animal.Animal;
 public class AnimalService {
     private Animal animal;
 
-    public AnimalService() {
+    public AnimalService(Animal animal) {
+        this.animal = animal;
     }
 
     public void printSpeed(int speed) {
