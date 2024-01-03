f72cf38d8841b906d21ce626ea02f68e5ac7daa8
minsu <zmfjtnl1@naver.com>
2024-01-03 20:11:51 +0900

    Upload AnimalClassfication

--- src/main/java/org/parkminsu/spring/annotation/AnimalClassfication.java
+++ src/main/java/org/parkminsu/spring/annotation/AnimalClassfication.java
@@ -1,4 +1,14 @@
 package org.parkminsu.spring.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
 public @interface AnimalClassfication {
+    String value();
 }
